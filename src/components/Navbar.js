086838aa951e81ccdc77f06ca30732064beedb67
NewIncome <alfredo_cardenas@live.com>
2021-02-11 23:31:19 -0600

    Remove comments and unused code in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,8 @@ import '../styles/navbar.css';
 const Navbar = props => {
   const { backLink, title } = props;
 
-  // if (title === 'Companies') {
-  //   return (
-  //     <nav id="navbar">
-  //       <a href="/" className="back-icon">{'<'}</a>
-  //       <div className="nav-title">{title}</div>
-  //     </nav>
-  //   );
-  // }
-
   return (
     <nav id="navbar">
-      {/* {title === 'Companies' ? <a href="/" className="back-icon">{'<'}</a>
-        : <div className="back-icon"><Link to={backLink}>{'<'}</Link></div>} */}
       <div className="back-icon"><Link to={backLink}>{'<'}</Link></div>
       <div className="nav-title">{title}</div>
     </nav>
